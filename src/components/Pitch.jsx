5ed2e8f363af03377e6fc3884582a3702b0a18ae
Lina Abuelezz <labuelez@uoguelph.ca>
2024-06-15 21:31:48 -0400

    font change

--- src/components/Pitch.jsx
+++ src/components/Pitch.jsx
@@ -2,58 +2,66 @@ import { useState, useContext } from "react";
 import { DialogueContext } from "../hooks/dialogueHook";
 
 const formations = [
-  { name: "4-3-3", positions: [
-      { type: "defender", position: "60px" },
-      { type: "defender", position: "170px" },
-      { type: "defender", position: "340px" },
-      { type: "defender", position: "450px" },
-      { type: "midfielder", position: "110px" },
-      { type: "midfielder", position: "258px" },
-      { type: "midfielder", position: "406px" },
-      { type: "forward", position: "90px" },
-      { type: "forward", position: "258px" },
-      { type: "forward", position: "426px" }
-    ]
+  {
+    name: "4-3-3",
+    positions: [
+      { type: "defender", position: "60px", id: "1" },
+      { type: "defender", position: "170px", id: "2" },
+      { type: "defender", position: "340px", id: "3" },
+      { type: "defender", position: "450px", id: "4" },
+      { type: "midfielder", position: "110px", id: "5" },
+      { type: "midfielder", position: "258px", id: "6" },
+      { type: "midfielder", position: "406px", id: "7" },
+      { type: "forward", position: "90px", id: "8" },
+      { type: "forward", position: "258px", id: "9" },
+      { type: "forward", position: "426px", id: "10" },
+    ],
   },
-  { name: "4-4-2", positions: [
-      { type: "defender", position: "60px" },
-      { type: "defender", position: "170px" },
-      { type: "defender", position: "340px" },
-      { type: "defender", position: "450px" },
-      { type: "midfielder", position: "60px" },
-      { type: "midfielder", position: "170px" },
-      { type: "midfielder", position: "340px" },
-      { type: "midfielder", position: "450px" },
-      { type: "forward", position: "130px" },
-      { type: "forward", position: "390px" }
-    ]
+  {
+    name: "4-4-2",
+    positions: [
+      { type: "defender", position: "60px", id: "1" },
+      { type: "defender", position: "170px", id: "2" },
+      { type: "defender", position: "340px", id: "3" },
+      { type: "defender", position: "450px", id: "4" },
+      { type: "midfielder", position: "60px", id: "5" },
+      { type: "midfielder", position: "170px", id: "6" },
+      { type: "midfielder", position: "340px", id: "7" },
+      { type: "midfielder", position: "450px", id: "8" },
+      { type: "forward", position: "130px", id: "9" },
+      { type: "forward", position: "390px", id: "10" },
+    ],
   },
-  { name: "3-4-3", positions: [
-      { type: "defender", position: "116px" },
-      { type: "defender", position: "258px" },
-      { type: "defender", position: "400px" },
-      { type: "midfielder", position: "60px" },
-      { type: "midfielder", position: "180px" },
-      { type: "midfielder", position: "330px" },
-      {type: "midfielder", position: "450px"},
-      { type: "forward", position: "100px" },
-      { type: "forward", position: "258px" },
-      {type: "forward", position: "410px"}
-    ]
+  {
+    name: "3-4-3",
+    positions: [
+      { type: "defender", position: "116px", id: "1" },
+      { type: "defender", position: "258px", id: "2" },
+      { type: "defender", position: "400px", id: "3" },
+      { type: "midfielder", position: "60px", id: "4" },
+      { type: "midfielder", position: "180px", id: "5" },
+      { type: "midfielder", position: "330px", id: "6" },
+      { type: "midfielder", position: "450px", id: "7" },
+      { type: "forward", position: "100px", id: "8" },
+      { type: "forward", position: "258px", id: "9" },
+      { type: "forward", position: "410px", id: "10" },
+    ],
   },
-  { name: "4-2-4", positions: [
-      { type: "defender", position: "60px" },
-      { type: "defender", position: "180px" },
-      { type: "defender", position: "330px" },
-      { type: "defender", position: "450px" },
-      { type: "midfielder", position: "130px" },
-      { type: "midfielder", position: "390px" },
-      { type: "forward", position: "60px" },
-      {type:"forward", position: "180px"},
-      { type: "forward", position: "330px" },
-      {type:"forward", position: "450px"}
-    ]
-  }
+  {
+    name: "4-2-4",
+    positions: [
+      { type: "defender", position: "60px", id: "1" },
+      { type: "defender", position: "180px", id: "2" },
+      { type: "defender", position: "330px", id: "3" },
+      { type: "defender", position: "450px", id: "4" },
+      { type: "midfielder", position: "130px", id: "5" },
+      { type: "midfielder", position: "390px", id: "6" },
+      { type: "forward", position: "60px", id: "7" },
+      { type: "forward", position: "180px", id: "8" },
+      { type: "forward", position: "330px", id: "9" },
+      { type: "forward", position: "450px", id: "10" },
+    ],
+  },
 ];
 
 const Pitch = () => {
@@ -61,19 +69,33 @@ const Pitch = () => {
   const [selectedFormation, setSelectedFormation] = useState(formations[0]);
 
   const handleFormationChange = (e) => {
-    const selected = formations.find(formation => formation.name === e.target.value);
+    const selected = formations.find(
+      (formation) => formation.name === e.target.value
+    );
     setSelectedFormation(selected);
   };
+
 
   return (
     <div className="flex">
       <div className="flex-1 p-4">
-        <select className="p-2 mb-2 mr-3 border-2 border-black" value={selectedFormation.name} onChange={handleFormationChange}>
+        <select
+          className="p-2 mb-2 mr-3 border-2 border-black"
+          value={selectedFormation.name}
+          onChange={handleFormationChange}
+        >
           {formations.map((formation, index) => (
-            <option key={index} value={formation.name}>{formation.name}</option>
+            <option key={index} value={formation.name}>
+              {formation.name}
+            </option>
           ))}
         </select>
-        <button className="font-mono text-xl bg-slate-500 p-2 rounded-xl hover:scale-110 text-white" onClick={openModal}>Add player</button>
+        <button
+          className="text-xl bg-emerald-500 p-2 rounded-xl hover:scale-110 text-white font-roboto-condensed font-normal"
+          onClick={openModal}
+        >
+          Add player
+        </button>
         <div className="border-2 mt-2"></div>
         <div>
           <ul></ul>
@@ -92,7 +114,10 @@ const Pitch = () => {
             <span
               key={index}
               className="absolute text-8xl hover:text-gray-300 cursor-pointer"
-              style={{ right: position.position, bottom: getVerticalPosition(position.type) }}
+              style={{
+                right: position.position,
+                bottom: getVerticalPosition(position.type),
+              }}
             >
               <strong>+</strong>
             </span>
@@ -109,15 +134,14 @@ const Pitch = () => {
   );
 };
 
-
 const getVerticalPosition = (type) => {
   switch (type) {
     case "defender":
-      return "125px"; 
+      return "125px";
     case "midfielder":
-      return "360px"; 
+      return "360px";
     case "forward":
-      return "550px"; 
+      return "550px";
     default:
       return "0";
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,3 +8,25 @@ export default {
   plugins: [],
 }
 
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto-condensed': ['"Roboto Condensed"', 'sans-serif'],
+      },
+      fontWeight: {
+        hairline: 100,
+        thin: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
+    },
+  },
+  plugins: [],
+};
